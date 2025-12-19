8ebe89a7a82d33563e97ff2304b2744cf57fdd4e
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2025-12-19 00:17:33 -0300

    Include more RSS feeds and send top 100 through email

--- web-scraping/lambda_function.py
+++ web-scraping/lambda_function.py
@@ -20,15 +20,12 @@ USER_INTERESTS = "Unexplained Mysteries, Historical Curiosities, Scientific Fact
 
 # The Curiosities Feed List
 RSS_FEEDS = [
-    # --- REDDIT (Inglês - Melhores para Shorts Visuais) ---
     "https://www.reddit.com/r/Damnthatsinteresting/top/.rss?t=day",
     "https://www.reddit.com/r/todayilearned/top/.rss?t=day",
     "https://www.reddit.com/r/ExplainLikeImFive/top/.rss?t=day",
     "https://www.reddit.com/r/Creepy/top/.rss?t=day",
     "https://www.reddit.com/r/OddlyTerrifying/top/.rss?t=day",
     "https://www.reddit.com/r/BeAmazed/top/.rss?t=day",
-
-    # --- CURIOSIDADES EM INGLÊS ---
     "https://listverse.com/feed/",
     "https://www.boredpanda.com/feed/",
     "https://www.thefactsite.com/feed/",
@@ -36,15 +33,57 @@ RSS_FEEDS = [
     "https://www.smithsonianmag.com/rss/smart-news/",
     "https://www.livescience.com/feeds/all",
     "https://www.damninteresting.com/feed/",
-
-    # --- BRASILEIROS (Curiosidades, História e Ciência) ---
     "https://super.abril.com.br/feed/",
     "https://super.abril.com.br/mundo-estranho/feed/",
     "https://gizmodo.uol.com.br/feed/",
     "https://aventurasnahistoria.com.br/feed/",
-    
-    # SUBSTITUTO: Canaltech Ciência (Excelente para Espaço/Astronomia/Descobertas)
     "https://canaltech.com.br/rss/ciencia/",
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UC7zbUfFoMAMGHIRUE8gjVnw", # TechTudo
+    "http://astronomy-universo.blogspot.com/feeds/posts/default", # Astronomia e Universo
+    "http://www.numaniaticos.com/feed/", # Numaniáticos
+    "https://api.reddit.com/subreddit/creepy", # r/creepy
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UCVVIVc6DfU8uWJ60vk8iKpQ", # Superinteressante
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UChz45Ir-nTCxn7qmJ5nXQ6Q", # GizmodoBR
+    "http://www.vocesabia.net/feed/", # Curiosidades no Você Sabia
+    "http://super.abril.com.br/blogs/oraculo/feed/", # Oráculo – Super
+    "https://hypescience.com/feed/", # HypeScience
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UCH2VZQBLFTOp6I_qgnBJCuQ", # Canal Nostalgia
+    "http://randomc.net/feed/", # Random Curiosity
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UClu474HMt895mVxZdlIHXEA", # Nerdologia
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UCunqx90PNIv30uOgn0m4CTg", # Acredite ou Não
+    "http://scienceblogs.com.br/colecionadores/feed/", # Colecionadores de Ossos
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UCNYaxPiba3oxmeL_3jKxnYA", # Insane Curiosity
+    "http://rss.megacurioso.com.br/feed", # Novidades do Mega Curioso
+    "http://gdata.youtube.com/feeds/base/users/superinteressanteweb/uploads?alt=rss&v=2&orderby=published&client=ytapi-youtube-profile", # Superinteressante (Legacy API)
+    "http://www.insoonia.com/feed", # iNSôÔNiA
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UCjcEWCkuafL02yvkujCwL4w", # Colecionadores de Ossos (YT)
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UC9li9QbQlmExYM6UxnvhuCQ", # That Creepy Reading
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UCj0O6W8yDuLg3iraAXKgCrQ", # Você Sabia?
+    "http://canalnostalgia.blogspot.com/feeds/posts/default", # Canal Nostalgia (Blog)
+    "http://mundoestranho.abril.com.br/rss", # Mundo Estranho – Super
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UCmUVDdwB1_RrQyfu80fvRrw", # Revista Galileu
+    "https://www.techtudo.com.br/rss/techtudo/", # techtudo
+    "http://gdata.youtube.com/feeds/base/users/iberethenorio/uploads?alt=rss&v=2&orderby=published&client=ytapi-youtube-profile", # Manual do Mundo (Legacy API)
+    "http://www.assombrado.com.br/feeds/posts/default", # Assombrado
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UCIQPHl1WKKTt9KkWyo_JNig", # INCRÍVEL
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UCKHhA5hN2UohhFDfNXB_cvQ", # Manual do Mundo
+    "https://psxbrasil.com.br/feed/", # PSX Brasil
+    "http://br.ign.com/feed.xml", # IGN Brasil
+    "http://gdata.youtube.com/feeds/base/users/canalmegacurioso/uploads?alt=rss&v=2&orderby=published&client=ytapi-youtube-profile", # Mega Curioso (Legacy API)
+    "http://misteriosdomundo.org/feed/", # Misterios do Mundo
+    "http://www.gizmodo.com.br/feed/", # Gizmodo em português
+    "http://www.eltiempo.com/contenido/mundo-curioso/rss.xml", # EL TIEMPO
+    "http://spacetoday.com.br/feed/", # SPACE TODAY
+    "https://revistagalileu.globo.com/rss/galileu", # galileu
+    "http://gdata.youtube.com/feeds/base/users/fecastanhari/uploads?alt=rss&v=2&orderby=published&client=ytapi-youtube-profile", # Canal Nostalgia (Legacy API)
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UCsg11Ullu0dBrNXTrDBKOXA", # Mega Curioso
+    "http://www.baratonta.com/feeds/posts/default", # Baratonta
+    "http://www.bbc.co.uk/portuguese/index.xml", # BBC Brasil
+    "http://super.abril.com.br/feed/", # Super
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UCn9Erjy00mpnWeLnRqhsA1g", # Ciência Todo Dia
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UCvKP7V9o8xfB-hpk3noKAiw", # Acervo do Terror
+    "http://www.hypeness.com.br/feed/", # Hypeness
+    "https://www.youtube.com/playlist?list=UUn9Erjy00mpnWeLnRqhsA1g" # Ciência Todo Dia (Playlist URL)
 ]
 
 def fetch_rss_items(url):
@@ -151,16 +190,16 @@ def lambda_handler(event, context):
             art['score'] = s['score']
             ranked.append(art)
             
-    # Sort Top 20 Descending
+    # Sort Top 100 Descending
     ranked.sort(key=lambda x: x.get('score', 0), reverse=True)
-    top_20 = ranked[:20]
+    top_100 = ranked[:100]
     
     # 4. Format Email
     date_str = datetime.now().strftime('%Y-%m-%d')
-    email_body = f"DAILY CURIOSITIES TOP 20 - {date_str}\n"
+    email_body = f"DAILY CURIOSITIES TOP 100 - {date_str}\n"
     email_body += "="*40 + "\n\n"
     
-    for i, art in enumerate(top_20, 1):
+    for i, art in enumerate(top_100, 1):
         email_body += f"{i}. [{art.get('score',0)}] {art['title']}\n"
         email_body += f"   {art['link']}\n\n"
         
@@ -171,7 +210,7 @@ def lambda_handler(event, context):
         sns = boto3.client('sns')
         sns.publish(
             TopicArn=SNS_TOPIC_ARN,
-            Subject=f"Top 20 Curiosities - {date_str}",
+            Subject=f"Top 100 Curiosities - {date_str}",
             Message=email_body
         )
         return {'statusCode': 200, 'body': 'Success! Email sent.'}
